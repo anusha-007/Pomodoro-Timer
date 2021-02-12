c5f7b22f5a56615f585077147d64b5c269ea988e
anusha <ramaduguanusha123@gmail.com>
2021-02-12 07:30:49 +0530

    pomodoro

--- pomodoro/src/Components/Pomodoro.jsx
+++ pomodoro/src/Components/Pomodoro.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 
 import styles from "./Pomodoro.module.css"
 
 export const Pomodoro = () => {
+    const [timer, useTimer] = useState(25)
+    const [workDuration, setWorkDuration] = useState(25)
+    const [breakDuration,, setBreakDuration] = useState(5)
+
+    // const [answer] = useTimer(timer)
+
+
+    const handleWorkDurInc = () =>{
+        
+            setWorkDuration( prev => prev + 5 )
+
+        
+        
+    }
+    const handleWorkDurDec = () =>{
+        setWorkDuration( prev => prev - 5)
+    }
+
+
     return (
         <div className = {styles.container}>
             <h1>Pomodoro Timer</h1>
             <div className = {styles.image_container}>
                  <div className = {styles.center}>
                      <div>
-                         <h3>keep working</h3>
-                         <h1>25 :00</h1>
+                         <span>keep working</span>
+                         <h1>{timer}</h1>
                          <button className = {styles.button}>start</button>
                      </div>
                  </div>
@@ -22,9 +41,15 @@ export const Pomodoro = () => {
 
                 <div className = {styles.work}>
                     <p>work duration</p>
-                    <button className = {styles.button_settings}>+</button>
-                    <div>5 mins</div>
-                    <button className = {styles.button_settings}>-</button>
+                    <button 
+                     onClick = { handleWorkDurInc}
+                     disabled = {workDuration >= 60}
+                     className = {styles.button_settings}>+</button>
+                    <div>{workDuration} mins</div>
+                    <button 
+                     onClick = {handleWorkDurDec}
+                     disabled  = {workDuration <= 5}
+                     className = {styles.button_settings}>-</button>
                 </div>
                 <div>
                 <button className = {styles.reset}>reset</button>
